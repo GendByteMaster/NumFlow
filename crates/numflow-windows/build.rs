bd0aa14e2ea464beab0aa9c73ad463726ff33a51
Gend <GendByteMaster@gmail.com>
2026-09-16 21:09:22 +0300

    style(windows): format UIAccess linker directive

--- crates/numflow-windows/build.rs
+++ crates/numflow-windows/build.rs
@@ -15,7 +15,9 @@ fn main() {
     .join("numflow-input.manifest");
 
     println!("cargo:rustc-link-arg-bin=numflow-input=/MANIFEST:EMBED");
-    println!("cargo:rustc-link-arg-bin=numflow-input=/MANIFESTUAC:level='asInvoker' uiAccess='true'");
+    println!(
+        "cargo:rustc-link-arg-bin=numflow-input=/MANIFESTUAC:level='asInvoker' uiAccess='true'"
+    );
     println!(
         "cargo:rustc-link-arg-bin=numflow-input=/MANIFESTINPUT:{}",
         manifest.display()
